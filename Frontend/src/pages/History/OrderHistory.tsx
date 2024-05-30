e300b9c655d73f25ab61353b925529af0fefe47d
maod boufass <maodboufass90@gmail.com>
2024-05-30 14:16:00 +0100

    Order History UI & LIsting  Order History & Searching Order History

--- Frontend/src/pages/History/OrderHistory.tsx
+++ Frontend/src/pages/History/OrderHistory.tsx
@@ -1,21 +1,46 @@
-import DefaultLayout from "../../layout/DefaultLayout"
-import { Breadcrumb } from "../Orders/components"
-import { Card } from 'primereact/card';
+import { Card } from "primereact/card";
+import DefaultLayout from "../../layout/DefaultLayout";
+import { Breadcrumb } from "../Orders/components";
+import { useEffect, useState } from "react";
+import {
+  useGetOrderHistoryQuery,
+  useSearchOrderHistoryQuery,
+} from "../../app/services/HistoryApiSlice";
+import { Accordion, AccordionTab } from "primereact/accordion";
+import { History } from "../../types/history";
 import HistoryOrderCard from "./components/HistoryOrderCard";
-import { useAppSelector } from "../../app/hooks";
 
+const ProductHistory = () => {
+  const { data, isLoading, isFetching, isSuccess } = useGetOrderHistoryQuery(
+    {}
+  );
+  const [history, setHistory] = useState<any>({});
+  const [SearchedHistory, setSearchedHistory] = useState<any>({});
+  const [query, setQuery] = useState<string>("");
 
-const OrderHistory = () => {
-  
-  const { orders } = useAppSelector((state) => state.orders);
+  const { data: searchData, isSuccess: isSearchSuccess } =
+    useSearchOrderHistoryQuery({ query }, { skip: !query || query == "" });
 
+  useEffect(() => {
+    if (isSuccess) {
+      setHistory(data.data);
+      console.log("Order Data => ", data.data);
+      
+    }
+  }, [data, isLoading, isFetching, isSuccess]);
 
+  useEffect(() => {
+    if (isSearchSuccess) {
+      setSearchedHistory(searchData.data);
+      console.log("setHistory (search) => ", SearchedHistory);
+    }
+  }, [searchData, isSearchSuccess]);
   return (
     <DefaultLayout>
       <Breadcrumb pageName="History" subPageName="Order" />
       <Card>
-        <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-          <h4 className="m-0">Manage Orders</h4>
+        <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between px-4">
+          <h1 className="text-2xl font-semibold ">Order History</h1>
           <div className="relative">
             <span className="absolute left-4.5 top-4 dark:text-white">
               <i className="pi pi-search" />
@@ -23,24 +48,44 @@ const OrderHistory = () => {
             <input
               type="search"
               placeholder="Search..."
-              className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary 
-              focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-              onChange={(e) => e}
+              className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none 
+              dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+              onChange={(e) => setQuery(e.target.value)}
             />
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-
-          {orders.map((order)=>(
-            <HistoryOrderCard 
-            order={order}
-            />
-          )) 
-          }
+        <div className="mt-8">
+          <Accordion>
+            {!query || query === "" ? (
+              Object.keys(history).map((date, index) => (
+                <AccordionTab key={index} header={date}>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                    {history[date].map((historyEntry: any) => (
+                      <HistoryOrderCard
+                        key={historyEntry.id}
+                        historyOrd={historyEntry}
+                      />
+                    ))}
+                  </div>
+                </AccordionTab>
+              ))
+            ) : (
+              <AccordionTab header="Search Resulte">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                  {SearchedHistory.map((historyEntry: History) => (
+                    <HistoryOrderCard
+                      key={historyEntry.id}
+                      historyOrd={historyEntry}
+                    />
+                  ))}
+                </div>
+              </AccordionTab>
+            )}
+          </Accordion>
         </div>
       </Card>
     </DefaultLayout>
   );
-}
+};
 
-export default OrderHistory
+export default ProductHistory;
--- Frontend/src/pages/History/components/HistoryOrderCard.tsx
+++ Frontend/src/pages/History/components/HistoryOrderCard.tsx
@@ -1,51 +1,40 @@
-import { FormatOrderStatus, getSeverity } from "../../../helpers/StatusFunctions";
-import { Order } from "../../../types/order";
+import { formatDateTime } from "../../../helpers/DateFormat";
+import { getActionTypeIcon ,getHistorySeverity, getSeverityColors } from "../../../helpers/HistoryActionType";
+import { History } from "../../../types/history";
+import HistoryOrderCardBody from "./HIstoryOrderCardBody";
 
 interface HistoryOrderCardProps {
-  order: Order;
+  historyOrd: History;
 }
 
-const HistoryOrderCard = ({ order }: HistoryOrderCardProps) => {
-  const Severity = (order: Order) => {
-    switch (order.status) {
-      case "pending":
-        return ["bg-orange-200", "bg-orange-600"];
+const HistoryOrderCard = ({historyOrd} : HistoryOrderCardProps) => {
 
-      case "paid":
-        return ["bg-green-200", "bg-green-600"];
-
-      case "partially_paid":
-        return ["bg-blue-200", "bg-blue-600"];
-
-      case "unpaid":
-        return ["bg-red-200", "bg-red-600"];
-
-      case "refunded":
-        return ["bg-purple-200", "bg-purple-600"];
-
-      case "partially_refunded":
-        return ["bg-gray-200", "bg-gray-600"];
-
-      default:
-        return ["bg-gray-200", "bg-gray-600"];
-    }
-  };
-
-  const severityClasses = Severity(order);
-  const FormatStatus = FormatOrderStatus(order);
+  const severityClasses = getSeverityColors(historyOrd.action_type);
 
   return (
     <div
-      className={`block max-w-[18rem] rounded-lg ${severityClasses[0]} text-meta-4 shadow-secondary-1 mx-auto `}
+      className={`block min-w-fit max-w-[18rem] rounded-lg ${severityClasses[0]} text-meta-4 shadow-secondary-1`}
     >
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
       />
       <div className="flex items-center gap-4 px-6 py-3 text-meta-4 ">
-        {order.status === "refunded" ? (
-          <span className={`material-symbols-outlined ${getSeverity(order)}`}>currency_exchange</span>
+        {historyOrd.action_type === "refunded" ? (
+          <span
+            className={`material-symbols-outlined ${getHistorySeverity(historyOrd.action_type)}`}
+          >
+            currency_exchange
+          </span>
         ) : (
+          <>{getActionTypeIcon(historyOrd.action_type)}</>
+        )}
+        <p className="text-xl font-semibold uppercase">
+          {historyOrd.initiator}
+        </p>
+      </div>
+      <div className="p-6">
+        <div className="flex gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -56,26 +45,31 @@ const HistoryOrderCard = ({ order }: HistoryOrderCardProps) => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className={`lucide lucide-pencil ${getSeverity(order)}`}
+            className="lucide lucide-calendar-clock"
           >
-            <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-            <path d="m15 5 4 4" />
-          </svg>)
-        }
-        <p className="text-xl font-semibold">Header</p>
-      </div>
-      <div className="p-6">
-        <h5 className="mb-2 text-xl font-medium leading-tight">
-          Primary card title
-        </h5>
-        <p className="text-base">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+            <path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5" />
+            <path d="M16 2v4" />
+            <path d="M8 2v4" />
+            <path d="M3 10h5" />
+            <path d="M17.5 17.5 16 16.3V14" />
+            <circle cx="16" cy="16" r="6" />
+          </svg>
+
+          {historyOrd.created_at ? (
+            <h5 className="mb-2 text-xl font-medium leading-tight text-wrap">
+              {formatDateTime(historyOrd.created_at)}
+            </h5>
+          ) : (
+            <h5 className="mb-2 text-xl font-medium leading-tight text-wrap">
+              {historyOrd.date}
+            </h5>
+          )}
+        </div>
+        <HistoryOrderCardBody history={historyOrd} />
         <div
           className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${severityClasses[1]} text-white mt-4`}
         >
-          {FormatStatus}
+          {historyOrd.action_type}
         </div>
       </div>
     </div>
